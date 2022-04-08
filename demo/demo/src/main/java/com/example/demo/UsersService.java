1c297cf4020d66799f96887f1cd37e1a6dcc5566
Tomasz Topor <71265170+neqts@users.noreply.github.com>
2022-04-08 11:03:08 +0200

    Add files via upload
    Lab 2 Zadania

---
+++ demo/demo/src/main/java/com/example/demo/UsersService.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersService {
+    public String getUsers(){
+        return "John, Matt, Chris";
+    }
+}
