f01a9ca7afe71d74f4406252ed6deab49f048f61
Tomasz Topor <71265170+neqts@users.noreply.github.com>
2022-05-13 18:24:52 +0200

    Lab 3 Exercises

---
+++ demo2/src/main/java/com/example/demo/PaginatedResult.java
@@ -0,0 +1,60 @@
+package com.example.demo;
+
+import java.util.Collection;
+import java.util.List;
+
+public class PaginatedResult {
+    public Integer pageNumber;
+    public Integer pagesCount;
+    public Integer pageSize;
+    public Integer totalCount;
+    public Collection<UserEntity> users;
+
+    public PaginatedResult(Integer pageNumber, Integer pagesCount, Integer pageSize, Integer totalCount, Collection<UserEntity> users) {
+        this.pageNumber = pageNumber;
+        this.pagesCount = pagesCount;
+        this.pageSize = pageSize;
+        this.totalCount = totalCount;
+        this.users = users;
+    }
+
+    public Integer getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(Integer pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public Integer getPagesCount() {
+        return pagesCount;
+    }
+
+    public void setPagesCount(Integer pagesCount) {
+        this.pagesCount = pagesCount;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public Integer getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(Integer totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public Collection<UserEntity> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Collection<UserEntity> users) {
+        this.users = users;
+    }
+}
